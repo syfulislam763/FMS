9e63c7bd3a067c220df979b15d886cd9d2672cb7
syfulislam763 <mahimkhan763@gmail.com>
2025-08-09 02:35:49 +0600

    android deleted

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import "./global.css";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-blue-500">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+
+    api.cache(true);
+
+    return {
+        presets: [
+            "nativewind/babel",
+            ["babel-preset-expo", {jsxImportSource: "nativewind"}],
+        ],
+    };
+
+
+};
