c3eb248f054d33231dca7917a1285e4e2934abaa
syfulislam763 <mahimkhan763@gmail.com>
2025-08-09 03:14:02 +0600

    font added

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import "./global.css";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
+import FontLoader from "./src/hooks/FontLoader";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-blue-500">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FontLoader>
+      <View style={styles.container}>
+        <Text style={{
+          fontFamily:'Archivo-ExtraBold',
+        }} className="text-blue-500">Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </FontLoader>
   );
 }
 
